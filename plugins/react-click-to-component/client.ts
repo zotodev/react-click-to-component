3d28d192a4ffdfea82bdedc484fdb700929b8c43
Shambhu Tiwary <devzoto@gmail.com>
2026-03-28 13:05:21 +0530

    Add click-to-component functionality for React
    This file implements a click-to-component feature for React, allowing users to interact with components and view their paths in the code. It includes style definitions for the context menu and event listeners for mouse movements and context menu actions.

---
+++ plugins/react-click-to-component/client.ts
@@ -0,0 +1,222 @@
+/** Inspired by https://github.com/ericclemmons/click-to-component */
+export {};
+
+const style = document.createElement("style");
+style.setAttribute("type", "text/css");
+style.setAttribute("data-vite-dev-id", "react-click-to-component");
+style.innerHTML = `[data-click-to-component-target] {
+  outline: auto 1px !important;
+}
+#click-to-component-menu {
+  position: fixed !important;
+  z-index: 1000;
+  margin-top: 8px !important;
+  margin-bottom: 8px !important;
+  background: #222 !important;
+  color: white !important;
+  padding: 8px !important;
+  border-radius: 6px !important;
+  font-size: 14px !important;
+  line-height: 1.5 !important;
+  display: flex !important;
+  gap: 2px !important;
+  overflow: auto !important;
+}
+.click-to-component-menu-item {
+  padding: 4px !important;
+  border-radius: 4px !important;
+  cursor: pointer !important;
+  display: flex !important;
+  justify-content: space-between !important;
+  gap: 8px !important;
+}
+.click-to-component-menu-item:hover {
+  background: #333 !important;
+}`;
+document.head.appendChild(style);
+
+// These will be replaced by the Vite plugin at load time
+const root = "__ROOT__";
+const base = "__BASE__";
+
+let currentTarget: HTMLElement | undefined;
+let hasMenu = false;
+const menuElement = document.createElement("div");
+menuElement.setAttribute("id", "click-to-component-menu");
+
+window.addEventListener("keyup", (event) => {
+  if (!event.altKey && (hasMenu || currentTarget)) cleanUp();
+});
+
+window.addEventListener("mousemove", (event) => {
+  if (!event.altKey) {
+    cleanUp();
+    return;
+  }
+  if (hasMenu) return;
+  if (!(event.target instanceof HTMLElement)) {
+    clearOverlay();
+    return;
+  }
+  if (event.target === currentTarget) return;
+  clearOverlay();
+  currentTarget = event.target;
+  event.target.dataset["clickToComponentTarget"] = "true";
+});
+
+const getMaxZIndex = (target: HTMLElement, current: number): number => {
+  const parent = target.parentElement;
+  if (!parent || parent === document.body) return current;
+  const zIndex = parseInt(window.getComputedStyle(parent).zIndex);
+  return getMaxZIndex(
+    parent,
+    isNaN(zIndex) ? current : Math.max(zIndex, current),
+  );
+};
+
+window.addEventListener("contextmenu", (event) => {
+  if (!event.altKey) return;
+  const target = event.target;
+  if (!(target instanceof HTMLElement)) return;
+  event.preventDefault();
+  const layers = getLayersForElement(target);
+  if (layers.length === 0) return;
+  const zIndex = getMaxZIndex(target, 999);
+  if (zIndex > 999) menuElement.style.zIndex = `${zIndex + 1}`;
+  const rect = target.getBoundingClientRect();
+  if (rect.bottom < window.innerHeight / 2) {
+    menuElement.style.top = `${rect.bottom}px`;
+    menuElement.style.bottom = "";
+    menuElement.style.maxHeight = `${window.innerHeight - rect.bottom - 16}px`;
+  } else if (rect.top > window.innerHeight / 2) {
+    menuElement.style.bottom = `${window.innerHeight - rect.top}px`;
+    menuElement.style.top = "";
+    menuElement.style.maxHeight = `${rect.top - 16}px`;
+  } else {
+    const bottomVisible = rect.bottom < window.innerHeight;
+    menuElement.style.bottom = `${
+      bottomVisible ? window.innerHeight - rect.bottom : 0
+    }px`;
+    menuElement.style.top = "";
+    menuElement.style.maxHeight = `${
+      (bottomVisible ? rect.bottom : window.innerHeight) - 16
+    }px`;
+  }
+  if (rect.left < window.innerWidth / 2) {
+    menuElement.style.left = `${rect.left}px`;
+    menuElement.style.right = "";
+  } else {
+    menuElement.style.right = `${window.innerWidth - rect.right}px`;
+    menuElement.style.left = "";
+  }
+  while (menuElement.firstChild) {
+    menuElement.removeChild(menuElement.firstChild);
+  }
+  menuElement.style.flexDirection = menuElement.style.top
+    ? "column"
+    : "column-reverse";
+  for (const layer of layers) {
+    const item = document.createElement("div");
+    item.className = "click-to-component-menu-item";
+    const spanL = document.createElement("span");
+    spanL.textContent = `<${layer.name} />`;
+    item.appendChild(spanL);
+    const spanR = document.createElement("span");
+    spanR.textContent = layer.path.replace(`${root}/`, "");
+    item.appendChild(spanR);
+    item.addEventListener("click", () => {
+      void fetch(
+        `${base}__open-in-editor?file=${encodeURIComponent(layer.path)}`,
+      );
+      cleanUp();
+    });
+    menuElement.appendChild(item);
+  }
+  if (!hasMenu) {
+    document.body.appendChild(menuElement);
+    hasMenu = true;
+  }
+});
+
+const cleanUp = () => {
+  clearOverlay();
+  removeMenu();
+};
+
+const clearOverlay = () => {
+  if (!currentTarget) return;
+  const current = document.querySelector("[data-click-to-component-target]");
+  if (current) delete (current as HTMLElement).dataset["clickToComponentTarget"];
+  currentTarget = undefined;
+};
+
+const removeMenu = () => {
+  if (!hasMenu) return;
+  document.body.removeChild(menuElement);
+  hasMenu = false;
+};
+
+const getLayersForElement = (element: Element) => {
+  let instance = getReactInstanceForElement(element);
+  const layers: { name: string; path: string }[] = [];
+  while (instance) {
+    const path = getPath(instance);
+    if (path) {
+      const name =
+        typeof instance.type === "string"
+          ? instance.type
+          : (instance.type.displayName ??
+            instance.type.name ??
+            instance.type.render?.name ??
+            "undefined");
+      layers.push({ name, path });
+    }
+    instance = instance._debugOwner;
+  }
+  return layers;
+};
+
+type Source = {
+  columnNumber?: number;
+  fileName: string;
+  lineNumber?: number;
+};
+
+type Fiber = {
+  _debugSource?: Source;
+  _debugInfo?: Source;
+  _debugOwner?: Fiber;
+  type:
+    | string
+    | { displayName?: string; name?: string; render?: () => unknown };
+};
+
+const getPath = (fiber: Fiber) => {
+  const source = fiber._debugSource ?? fiber._debugInfo;
+  if (!source) {
+    console.debug("Couldn't find a React instance for the element", fiber);
+    return;
+  }
+  const { columnNumber = 1, fileName, lineNumber = 1 } = source;
+  return `${fileName}:${lineNumber}:${columnNumber}`;
+};
+
+const getReactInstanceForElement = (element: Element): Fiber | undefined => {
+  if ("__REACT_DEVTOOLS_GLOBAL_HOOK__" in window) {
+    const { renderers } = (window as any).__REACT_DEVTOOLS_GLOBAL_HOOK__;
+    for (const renderer of renderers.values()) {
+      try {
+        const fiber = renderer.findFiberByHostInstance(element);
+        if (fiber) return fiber;
+      } catch {
+        // React may be mid-render
+      }
+    }
+  }
+  if ("_reactRootContainer" in element) {
+    return (element as any)._reactRootContainer._internalRoot.current.child;
+  }
+  for (const key in element) {
+    if (key.startsWith("__reactFiber")) return (element as any)[key];
+  }
+};
